205ea850526566a0564cdfae9381c724caaa6ee1
ralph <hwibaski@gmail.com>
2023-11-07 22:25:04 +0900

    middleware

--- nest/cf-sns/src/posts/posts.module.ts
+++ nest/cf-sns/src/posts/posts.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,6 +13,7 @@ import { UsersModule } from 'src/users/users.module';
 import { CommonModule } from 'src/common/common.module';
 import { ImageModel } from 'src/common/entity/image.entity';
 import { PostImagesService } from './image/images.service';
+import { LogMiddleware } from 'src/common/middleware/log.middleware';
 
 @Module({
   imports: [
@@ -19,4 +25,10 @@ import { PostImagesService } from './image/images.service';
   controllers: [PostsController],
   providers: [PostsService, PostImagesService],
 })
-export class PostsModule {}
+export class PostsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LogMiddleware)
+      .forRoutes({ path: 'posts', method: RequestMethod.ALL });
+  }
+}
