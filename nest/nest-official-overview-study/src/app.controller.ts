986615dc18c0671bdbb536454b0bbb6f61be5a6c
ralph <hwibaski@gmail.com>
2023-11-21 22:08:34 +0900

    [nest-official-study] controller

--- nest/nest-official-overview-study/src/app.controller.ts
+++ nest/nest-official-overview-study/src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HostParam,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
+@Controller({ host: 'localhost111' })
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@HostParam() host, @Req() req): string {
+    console.log(req.hostname);
+    console.log(host);
     return this.appService.getHello();
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
